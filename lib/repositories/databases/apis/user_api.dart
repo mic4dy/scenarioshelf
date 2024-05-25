af3beb0e6aa5fcc0078672405fd59abb3a1fef67
micady <micady.dev@gmail.com>
2024-05-26 02:34:09 +0900

    feat: supabase migration

--- lib/repositories/databases/apis/user_api.dart
+++ lib/repositories/databases/apis/user_api.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/foundation.dart';
 import 'package:scenarioshelf/models/user/user.dart';
 
+// ignore: one_member_abstracts
 abstract interface class UserAPI {
   Future<User> update({
     required String name,
